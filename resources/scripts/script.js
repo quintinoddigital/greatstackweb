043b21f386b404833cdbb8a4b0ab54ffbc2d51df
Quintino Digital <quintino.digital@outlook.com.br>
2024-04-09 16:18:03 -0300

    feature: Fechar submenu ao clicar em qualquer local da tela

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -3,3 +3,11 @@ const submenuInput = document.getElementById("submenu");
 function abrirSubmenu() {
     submenuInput.classList.toggle("abrir-menu");
 }
+
+window.document.addEventListener("click", function(event) {
+    const submenu = document.getElementById("submenu");
+    const perfil = document.querySelector(".perfil");
+    if (!submenu.contains(event.target) && event.target !== perfil) {
+        submenu.classList.remove("abrir-menu");
+    }
+});
